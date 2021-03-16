3e65f7634bf02f78294aaa3dd16e78248f9f70c0
alexd98 <53147460+alexd98@users.noreply.github.com>
2021-03-16 20:21:40 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementByClassName("eventbutton").addEventListener("click", clickFunction);
+
+function clickFunction() {
+    document.getElementsByClassName("eventbutton").innerHTML="Added to Basket"
+};
